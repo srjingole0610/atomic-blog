3dbabdfdf18a3a7455692a42e4369c193f5471dd
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2026-02-16 18:39:39 +0530

    Add App-memo component and refactor App to use context for post management

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
+import AppMemo from "./App-memo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <App /> */}
+    <AppMemo />
+  </React.StrictMode>,
 );
